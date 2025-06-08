be4933005d13051129c82345835f369cb6b68630
Johnathan Falk <johnathan.falk@gmail.com>
2025-06-07 21:58:43 -0400

    Add issue updates for batch command

---
+++ cmd/batch_test.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/spf13/viper"
+	"subtitle-manager/pkg/translator"
+)
+
+// TestBatchCmd verifies the batch command translates multiple files concurrently.
+func TestBatchCmd(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, `{"data":{"translations":[{"translatedText":"hola"}]}}`)
+	}))
+	defer srv.Close()
+	translator.SetGoogleAPIURL(srv.URL)
+	defer translator.SetGoogleAPIURL("https://translation.googleapis.com/language/translate/v2")
+
+	dir := t.TempDir()
+	inputs := []string{}
+	for i := 0; i < 2; i++ {
+		in := filepath.Join(dir, fmt.Sprintf("in%d.srt", i))
+		data, _ := os.ReadFile("../testdata/simple.srt")
+		os.WriteFile(in, data, 0644)
+		inputs = append(inputs, in)
+	}
+
+	viper.Set("translate_service", "google")
+	viper.Set("google_api_key", "k")
+	viper.Set("openai_api_key", "")
+	viper.Set("grpc_addr", "")
+	defer viper.Reset()
+	batchCmd.Flags().Parse([]string{"--workers=2"})
+	if err := batchCmd.RunE(batchCmd, append([]string{"es"}, inputs...)); err != nil {
+		t.Fatalf("batch: %v", err)
+	}
+	for _, in := range inputs {
+		out := strings.TrimSuffix(in, filepath.Ext(in)) + ".es.srt"
+		data, err := os.ReadFile(out)
+		if err != nil {
+			t.Fatalf("read out: %v", err)
+		}
+		if !strings.Contains(string(data), "hola") {
+			t.Fatalf("missing translation in %s", out)
+		}
+	}
+}
