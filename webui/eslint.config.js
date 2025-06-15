04232e071632a23bfa2a11fe6011f9aaac78ad4d
Johnathan Falk <johnathan.falk@gmail.com>
2025-06-14 20:23:16 -0400

    refactor(eslint): reorganize globals configuration
    Files changed:
    - Updated globals configuration to spread browser and node globals: [webui/eslint.config.js](https://github.com/owner/repo/blob/branch/webui/eslint.config.js)
    - Added jest globals for test files: [webui/eslint.config.js](https://github.com/owner/repo/blob/branch/webui/eslint.config.js)
    - Improved readability of globals structure: [webui/eslint.config.js](https://github.com/owner/repo/blob/branch/webui/eslint.config.js)
    
    Signed-off-by: Johnathan Falk <johnathan.falk@gmail.com>

--- webui/eslint.config.js
+++ webui/eslint.config.js
@@ -1,7 +1,7 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 
 export default [
   { ignores: ['dist'] },
@@ -9,7 +9,10 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
@@ -23,11 +26,41 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]', argsIgnorePattern: '^_' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+    },
+  },
+  // Test files configuration
+  {
+    files: ['**/__tests__/**/*.{js,jsx}', '**/*.test.{js,jsx}', '**/*.spec.{js,jsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+        describe: 'readonly',
+        it: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+        vi: 'readonly',
+      },
+    },
+  },
+  // Playwright config
+  {
+    files: ['playwright.config.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        process: 'readonly',
+      },
     },
   },
 ]
