ef6b3bdbd6ed37859e2a6ce4136c6685e2aee38d
Johnathan Falk <johnathan.falk@gmail.com>
2025-06-16 20:19:54 -0400

    fix: Update generate.go

--- pkg/translator/generate.go
+++ pkg/translator/generate.go
@@ -1,4 +1,4 @@
 package translator
 
 //go:generate go run github.com/vektra/mockery/v2 --name=GoogleClient --output=mocks --outpkg=mocks --filename=google_client.go
-//go:generate go run github.com/vektra/mockery/v2 --name=OpenAIClient --output=mocks --outpkg=mocks --filename=openai_client.go
+
