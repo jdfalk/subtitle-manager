ada297e3582a5a8ebc825130d50a55b8a96342f1
Johnathan Falk <johnathan.falk@gmail.com>
2025-06-22 20:01:04 -0400

    chore: track rename command issue
    Files changed:
    - new issue update: [.github/issue-updates/598b6e73-f566-417e-a265-f27583ffea34.json](.github/issue-updates/598b6e73-f566-417e-a265-f27583ffea34.json)

---
+++ cmd/rename.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/jdfalk/subtitle-manager/pkg/logging"
+	"github.com/jdfalk/subtitle-manager/pkg/renamer"
+)
+
+// renameCmd renames subtitle files to match a video filename.
+var renameCmd = &cobra.Command{
+	Use:   "rename [video] [lang]",
+	Short: "Rename subtitle to match video file",
+	Args:  cobra.ExactArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		logger := logging.GetLogger("rename")
+		video, lang := args[0], args[1]
+		if err := renamer.Rename(video, lang); err != nil {
+			logger.Warnf("rename %s: %v", video, err)
+			return err
+		}
+		logger.Infof("renamed subtitles for %s", video)
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(renameCmd)
+}
